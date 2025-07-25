67d37e2de3197617add4f817d151d059958014a3
katsura919 <katsuragik919@gmail.com>
2025-07-25 18:51:25 +0800

    remove user module and associated routes and views

--- app/user/index.js
+++
@@ -1,8 +0,0 @@
-// User app module exports
-// This follows Django-like app structure where each app has its own views and URLs
-
-const userUrls = require('./urls');
-
-module.exports = {
-  urls: userUrls,
-};
